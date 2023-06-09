ce171da8c8d13932ac35df5904b6c4e91d473dea
Andrew Kulakov <irlanderhelp@gmail.com>
2023-06-09 22:25:11 +0300

    Создание гита

---
+++ Seminar1/task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+// a = 25, b = 5 -> да 
+// a = 2, b = 10 -> нет 
+// a = 9, b = -3 -> да 
+// a = -3, b = 9 -> нет
+// 3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 	3 -> Среда 
+// 5 -> Пятница
+using System;
+
+Console.WriteLine("Введите первое число:");
+string? numberStringOne = Console.ReadLine();
+
+Console.WriteLine("Введите второе число:");
+string? numberStringTwo = Console.ReadLine();
+
+int numberIntOne = Convert.ToInt32(numberStringOne);
+int numberIntTwo = Convert.ToInt32(numberStringTwo);
+
+if (numberIntTwo * numberIntTwo == numberIntOne)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
---
+++ Seminar1/task5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+Console.WriteLine("Введите число:");
+string? numberStringN = Console.ReadLine();
+int numberN = Convert.ToInt32(numberStringN);
+for (int i = -numberN; i <= numberN; i++)
+{
+    Console.Write(i);
+    if (i != numberN)
+        {
+        Console.Write(", ");
+        }
+}
