c6533b45ea0d5af18aeda6b69ce33205b7d402fa
Andrew Kulakov <irlanderhelp@gmail.com>
2023-06-13 19:43:50 +0300

    Домашнее задание 4

--- Seminar1/task4/Program.cs
+++ Seminar1/task4/Program.cs
@@ -8,14 +8,21 @@ string? numberStringOne = Console.ReadLine();
 Console.WriteLine("Введите второе число:");
 string? numberStringTwo = Console.ReadLine();
 
+Console.WriteLine("Введите третье число:");
+string? numberStringThree = Console.ReadLine();
+
 int numberIntOne = Convert.ToInt32(numberStringOne);
 int numberIntTwo = Convert.ToInt32(numberStringTwo);
+int numberIntThree = Convert.ToInt32(numberStringThree);
 
 if (numberIntOne >= numberIntTwo)
 {
-    Console.WriteLine("Max = "+ numberIntOne);
+    numberIntTwo = numberIntOne;
 }
-else
+
+if (numberIntTwo >= numberIntThree)
 {
-    Console.WriteLine("Max = "+ numberIntTwo);
+    numberIntThree = numberIntTwo;
 }
+
+ Console.WriteLine("Max = "+ numberIntThree);
