9585bf54ea227d56a4f4ae711f3a736c524f3b9b
Andrew Kulakov <irlanderhelp@gmail.com>
2023-06-13 19:35:08 +0300

    Домашнее задание 2

---
+++ Seminar1/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+using System;
+
+Console.WriteLine("Введите первое число:");
+string? numberStringOne = Console.ReadLine();
+
+Console.WriteLine("Введите второе число:");
+string? numberStringTwo = Console.ReadLine();
+
+int numberIntOne = Convert.ToInt32(numberStringOne);
+int numberIntTwo = Convert.ToInt32(numberStringTwo);
+
+if (numberIntOne >= numberIntTwo)
+{
+    Console.WriteLine("Max = "+ numberIntOne);
+}
+else
+{
+    Console.WriteLine("Max = "+ numberIntTwo);
+}
---
+++ Seminar1/task4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+using System;
+
+Console.WriteLine("Введите первое число:");
+string? numberStringOne = Console.ReadLine();
+
+Console.WriteLine("Введите второе число:");
+string? numberStringTwo = Console.ReadLine();
+
+int numberIntOne = Convert.ToInt32(numberStringOne);
+int numberIntTwo = Convert.ToInt32(numberStringTwo);
+
+if (numberIntOne >= numberIntTwo)
+{
+    Console.WriteLine("Max = "+ numberIntOne);
+}
+else
+{
+    Console.WriteLine("Max = "+ numberIntTwo);
+}
