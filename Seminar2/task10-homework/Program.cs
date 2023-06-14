438934e2c275bbf1b6880bca917b21d447aa1dbd
Andrew Kulakov <irlanderhelp@gmail.com>
2023-06-14 19:48:16 +0300

    семинар 2 домашнее задание 10

---
+++ Seminar2/task10-homework/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (100 <= number && number <= 999)
+{
+    number = number / 10;
+    number = number % 10;
+    Console.WriteLine($"Вторая цифра равна {number}");
+}
+else
+{
+    Console.WriteLine("Ошибка ввода");
+}
