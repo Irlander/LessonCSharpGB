035ff5d2fc519ec26fd08b8175d8b8dfc55894ab
Andrew Kulakov <irlanderhelp@gmail.com>
2023-06-17 01:00:08 +0300

    семинар 3 добавление альтернативного решения

---
+++ Seminar3/task19-v1-homework/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 19: Напишите программу, которая принимает 
+// на вход пятизначное число и проверяет, является ли 
+// оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 10000 || number > 99999)
+{
+    Console.WriteLine("Число содержит менее или более 5 цифр");
+}
+else
+{
+    int numberFirst = number / 10000;
+    int numberFive = number % 10;
+    number = (number - numberFirst * 10000) / 10;
+    int numberTwo = number / 100;
+    int numberFour = number % 10;
+    if (numberFirst == numberFive && numberTwo == numberFour)
+    {
+        Console.WriteLine("Число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиндромом");
+    }
+}
---
+++ Seminar3/task19-v2-homework/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 19: Напишите программу, которая принимает 
+// на вход пятизначное число и проверяет, является ли 
+// оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 10000 || number > 99999)
+{
+    Console.WriteLine("Число содержит менее или более 5 цифр");
+}
+else
+{
+    int origin = number;
+    int temp = number % 10;
+    number = number / 10;
+    int palindrome = temp;
+    while (number > 0)
+    {
+        temp = number % 10;
+        number = number / 10;
+        palindrome = palindrome * 10 + temp;
+    }
+    if (origin == palindrome)
+    {
+        Console.WriteLine($"Число {origin} является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"Число {origin} не является палиндромом");
+    }
+}
