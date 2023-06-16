852d7eb5f6622e6908476d3f708c1d862748bdc7
Andrew Kulakov <irlanderhelp@gmail.com>
2023-06-16 21:47:39 +0300

    семинар 3 начало

---
+++ Seminar3/task18/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти");
+int part = Convert.ToInt32(Console.ReadLine());
+
+if (part == 1)
+{
+ Console.WriteLine("x>1, y>1");
+}
+else if (part == 2)
+{
+ Console.WriteLine("x<1, y>1");
+}
+else if (part == 3)
+{
+ Console.WriteLine("x<1, y<1");
+}
+else if (part == 4)
+{
+ Console.WriteLine("x>1, y<1");
+}
+else
+{
+ Console.WriteLine("Такой области нет");
+}
