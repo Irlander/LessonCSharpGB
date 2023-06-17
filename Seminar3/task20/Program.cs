10970adec08032df910a121bd52f53cec7591890
Andrew Kulakov <irlanderhelp@gmail.com>
2023-06-17 20:34:20 +0300

    семинар 3 решение домашнего задания

--- Seminar3/task20/Program.cs
+++ Seminar3/task20/Program.cs
@@ -1,2 +1,13 @@
 ﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
 
+Console.WriteLine("Введите координату первой точки по оси x");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату первой точки по оси y");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату второй точки по оси x");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату второй точки по оси y");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine(result);
---
+++ Seminar3/task21-homework/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 21: Напишите программу, которая принимает 
+// на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите координату первой точки по оси x");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату первой точки по оси y");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату первой точки по оси z");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату второй точки по оси x");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату второй точки по оси y");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату второй точки по оси z");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine($"{result:f2}");
