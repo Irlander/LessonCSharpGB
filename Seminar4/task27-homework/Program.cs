225bfd6355e1dcf6d30d613e333506fbebacce90
Andrew Kulakov <irlanderhelp@gmail.com>
2023-06-21 21:39:42 +0300

    семинар 4 задача 27

--- Seminar4/task27-homework/Program.cs
+++ Seminar4/task27-homework/Program.cs
@@ -2,3 +2,19 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+int SumNumber()
+{
+    Console.WriteLine("Введите целое число:");
+    int value = Convert.ToInt32(Console.ReadLine());
+    int sum = 0;
+    while (value > 0)
+    {
+        sum = sum + value % 10;
+        value = value / 10;
+    }
+    return sum;
+}
+
+int enter = SumNumber();
+Console.WriteLine(enter);
