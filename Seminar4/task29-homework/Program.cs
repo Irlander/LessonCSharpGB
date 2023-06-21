698f91f7b2b09e3599b33bcce6c3b51467d52a8c
Andrew Kulakov <irlanderhelp@gmail.com>
2023-06-21 21:55:00 +0300

    семинар 4 задача 29 + комментарии

--- Seminar4/task29-homework/Program.cs
+++ Seminar4/task29-homework/Program.cs
@@ -1,1 +1,12 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+//объявляем массив
+int[] array = new int [8];
+//цикл с заполнением данными
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Введите целое число для ячейки с индексом {i} в массиве");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+//вывод информации
+Console.WriteLine($"Массив заполнен: {array[0]}, {array[1]}, {array[2]}, {array[3]}, {array[4]}, {array[5]}, {array[6]}, {array[7]}");
