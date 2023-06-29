235cd3bb9b151573faef020a17f7e368198d908c
Andrew Kulakov <irlanderhelp@gmail.com>
2023-06-29 20:30:51 +0300

    семинар 6

---
+++ Seminar6/task39/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int[] GetRandomArray(int index, int leftRange, int rightRange) // функция получения массива
+{
+    int[] array = new int[index];  //создаём новый массив с длиной равной index
+
+    for (int i = 0; i < array.Length; i++)  // цикл заполняет массив случайными  значениями от leftRange до rightRange + 1
+    {
+        array[i] = Random.Shared.Next(leftRange, rightRange + 1);
+    }
+
+    return array; //возвращение массива
+}
+
+void ReverseArray(int[] array) //функция изменения массива (void возвращает то, что было на входе)
+{
+    for (int i = 0; i < array.Length / 2; i++) //цикл, который перебирает все индексы до середины и меняет их местами со значениями с конца
+    {
+        int temp = array[i];  //временная переменная
+        array[i] = array[array.Length - 1 - i]; //переписываем значение индекса с конца в начало
+        array[array.Length - 1 - i] = temp; //переписываем значение из темпа в конец
+    }
+}
+
+int[] myArray = GetRandomArray(8, -10, 10);  //создание массива с указанными параметрами
+Console.WriteLine(string.Join(", ", myArray)); //вывод изначального массива
+ReverseArray(myArray); //разворот массива
+Console.WriteLine(string.Join(", ", myArray)); //вывод изменённого массива
---
+++ Seminar6/task40/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник со сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+Console.WriteLine("Введите длину первой стороны:"); //ввод сторон
+int edgeOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину второй стороны:");
+int edgeTwo = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину третьей стороны:");
+int edgeThree = Convert.ToInt32(Console.ReadLine());
+
+// проверка условия, что сумма любых двух строн больше одной стороны
+if (edgeOne < edgeTwo + edgeThree && edgeTwo < edgeOne + edgeThree && edgeThree < edgeTwo + edgeOne)
+{
+    Console.WriteLine("Треугольник с такими сторонами может существовать.");
+}
+else
+{
+    Console.WriteLine("Треугольник с такими сторонами не может существовать.");
+}
