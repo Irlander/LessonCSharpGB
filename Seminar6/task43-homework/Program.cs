5e2283ebc48353284897a78b86ebb7b7b155e579
Andrew Kulakov <irlanderhelp@gmail.com>
2023-06-29 22:33:39 +0300

    семинар 6 домашнее задание 43

--- Seminar6/task43-homework/Program.cs
+++ Seminar6/task43-homework/Program.cs
@@ -2,3 +2,20 @@
 // y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+Console.WriteLine("Введите угловой коэффициент первой прямой"); //вводим данные
+int coeffFirstLine = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите точку пересечения с осью Y первой прямой");
+int crossfFirstLine = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите угловой коэффициент второй прямой");
+int coeffSecondLine = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите точку пересечения с осью Y второй прямой");
+int crossfSecondLine = Convert.ToInt32(Console.ReadLine());
+
+// считаем координату пересечения х по формуле
+double xCoordinate = (Convert.ToDouble(crossfSecondLine) - Convert.ToDouble(crossfFirstLine)) / 
+                     (Convert.ToDouble(coeffFirstLine) - Convert.ToDouble(coeffSecondLine));
+
+// считаем координату пересечения у по формуле
+double yCoordinate = Convert.ToDouble(coeffFirstLine) * xCoordinate + Convert.ToDouble(crossfFirstLine);
+
+Console.WriteLine($"Координаты точки пересечения: {xCoordinate}, {yCoordinate}"); // вывод
