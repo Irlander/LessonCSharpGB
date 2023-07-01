10f1f5a9a0e68d92da27768e18b95d9736bee276
Andrew Kulakov <irlanderhelp@gmail.com>
2023-07-01 23:40:48 +0300

    семинар 7 домашнее задание 47

--- Seminar7/task47/Program.cs
+++ Seminar7/task47/Program.cs
@@ -4,3 +4,34 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+const int ROWS = 3; // объявляем константы
+const int COLUMNS = 4;
+
+double[,] GetRandomMatrix(int rows, int columns) // метод создания массива с заданными параметрами
+{
+    double[,] matrix = new double[rows, columns]; // создание массива с нужным количеством строк и столбцов
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(-10, 11) + Math.Round(Random.Shared.NextDouble(), 1); // заполнение значениями
+        }
+    }
+
+    return matrix; // возвращение массива
+}
+
+void PrintMatrix(double[,] matrix) // метод вывода в консоль массива
+{
+    for(int i = 0; i < matrix.GetLength(0); i++) // вывод строк
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++) // вывод столбцов
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] myMatrix = GetRandomMatrix(ROWS, COLUMNS); 
+PrintMatrix(myMatrix);
