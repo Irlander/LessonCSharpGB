55a01e44edc381e34e96ad0d4aa50bd5c5f015d0
Andrew Kulakov <irlanderhelp@gmail.com>
2023-07-06 23:26:51 +0300

    семинар 8 домашнее задание 58

---
+++ Seminar8/task58-homework v2/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 6 16
+// 9 6
+
+const int ROWS = 3; // объявляем константы
+const int COLUMNS = 4;
+
+int[,] GetRandomMatrix(int rows, int columns) // функция создания массива с заданными параметрами
+{
+    int[,] matrix = new int[rows, columns]; // создание массива с нужным количеством строк и столбцов
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(1, 10); // заполнение значениями
+        }
+    }
+
+    return matrix; // возвращение массива
+}
+
+void PrintMatrix(int[,] matrix) // функция вывода в консоль массива
+{
+    for(int i = 0; i < matrix.GetLength(0); i++) // вывод строк
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++) // вывод столбцов
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MultiplyMatrix(int[,] matrixOne, int[,] matrixTwo) // функция перемножения матриц
+{
+    for(int i = 0; i < matrixOne.GetLength(0); i++) // перебор строк
+    {
+        for(int j = 0; j < matrixOne.GetLength(1); j++) // перебор столбцов
+        {
+            matrixOne[i, j] = matrixOne[i, j] * matrixTwo[i, j]; // перемножаем значения по индексам в матрицах
+        }
+    }
+}
+
+int[,] myMatrixOne = GetRandomMatrix(ROWS,COLUMNS); // создаём первую матрицу
+int[,] myMatrixTwo = GetRandomMatrix(ROWS,COLUMNS); // создаём вторую матрицу
+PrintMatrix(myMatrixOne); // вывод первой матрицу
+Console.WriteLine(); // отступ
+PrintMatrix(myMatrixTwo); // вывод вторую матрицу
+Console.WriteLine(); // отступ
+MultiplyMatrix(myMatrixOne, myMatrixTwo); // перемножаем значения
+PrintMatrix(myMatrixOne); // вывод
